183b162f2e7e5410c990f064aea6fb5241cf992a
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2024-12-17 20:01:31 -0300

    feat: status in client contact tables

---
+++ prisma/migrations/20241217230047_add_status_in_tables/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ContatePorEmail_Cliente" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "ModeloEspecifico_Cliente" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT false;
